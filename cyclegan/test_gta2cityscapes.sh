27fd3b07487a3e8474fca4748e0cd18503eda391
Shreyas Niradi <shreyas.niradi@gmail.com>
2021-03-07 01:48:06 -0800

    quick changes to train/test scripts

--- cyclegan/test_gta2cityscapes.sh
+++ cyclegan/test_gta2cityscapes.sh
@@ -1,7 +1,10 @@
 CUDA_VISIBLE_DEVICES=0 python test.py --name gta2cityscapes \
-    --resize_or_crop="scale_width_and_crop" \
-    --loadSize=1024 --fineSize=1024 \
+    --resize_or_crop="crop" \
+    --loadSize=1920 --fineSize=1920 \
+    --how_many 50 \
+    --which_epoch 20 \
     --model cycle_gan \
     --batchSize 1 \
     --dataset_mode unaligned --dataroot /mnt/data/cyclegan_data/ \
     --which_direction AtoB
+    --no_html
--- cyclegan/train_gta2cityscapes.sh
+++ cyclegan/train_gta2cityscapes.sh
@@ -1,8 +1,16 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --name gta2cityscapes \
-    --resize_or_crop="scale_width_and_crop" \
-    --loadSize=1024 --fineSize=400 \
-    --model cycle_gan \
-    --lambda_identity 1.0 \
-    --batchSize 1 \
-    --dataset_mode unaligned --dataroot /mnt/data/cyclegan_data/ \
-    --which_direction AtoB
+CUDA_VISIBLE_DEVICES=0 python train.py --name gta2cityscapes 
+                                       --resize_or_crop='scale_width_and_crop' 
+                                       --loadSize=1024 
+                                       --fineSize=400 
+                                       --model cycle_gan 
+                                       --lambda_identity 1.0 
+                                       --batchSize 1 
+                                       --dataset_mode unaligned 
+                                       --dataroot /mnt/data/cyclegan_data/ 
+                                       --which_direction AtoB 
+                                       --display_id=0 
+                                       --checkpoints_dir='/ceph/cycada/cyclegan_checkpoints/' 
+                                       --save_epoch_freq=1 
+                                    #    --continue_train 
+                                    #    --which_epoch=12 
+                                    #    --epoch_count=12
