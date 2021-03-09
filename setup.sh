b56b94a8a9d09b129d58f80204a8d68ade6b3c29
raviteja.kvns <raviteja.kvns@gmail.com>
2021-03-08 20:26:59 -0800

    Got UPSNet running

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ export PYTHONPATH=".:${PYTHONPATH} "
 
 # Setting up necessary python paths to link the modules of upsnet and cycada works
 export PYTHONPATH="$(pwd)/UPSNet/:$(pwd)/:${PYTHONPATH}"
-
+export CITYSCAPES_DATASET="$(pwd)/data/cityscapes"
 # Dependencies
 # CYCADA
 pip install -r requirements.txt
